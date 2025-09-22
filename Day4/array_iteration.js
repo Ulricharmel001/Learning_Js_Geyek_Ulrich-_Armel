188244bd58c341ffb8ae4475aabcd6779229b1f5
Geyek Ulrich Armel <ulrichsgraphix@gmail.com>
2025-09-22 15:46:51 +0100

    iterating an array in iterate_array.j file

---
+++ Day4/array_iteration.js
@@ -0,0 +1,11 @@
+// array iteration 
+var Food =  ['beans', 'rice', 'eru']
+console.log(Food.length);
+
+console.log(Food[0]);
+console.log(Food[1]);
+for(var i =0; i<Food.length; i++){
+    console.log(Food[i]);
+}
+
+
