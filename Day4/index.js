290d00d6937193ac2b060807fb071d5af8d3b73f
Geyek Ulrich Armel <ulrichsgraphix@gmail.com>
2025-09-22 13:00:41 +0100

    innitial commit,  created index.js, started learning basic js , declaring data, working with different data types in javascript

---
+++ Day4/index.js
@@ -0,0 +1,31 @@
+// woking with variables
+
+"Ulrich"
+var name = "Ulrich"
+console.log("Hello", name)
+
+
+//operators
+console.log(2+4);
+console.log(2+24+7)
+console.log(2*3)
+console.log(3-1)
+console.log(4/2)
+console.log(3<5)
+
+console.log(43%5)
+
+//strings 
+// collection of characters enclose in single of double quotation mark
+
+console.log("i am a string")
+// Boolean 
+// has only two value True and false
+
+console.log(1<2)
+
+console.log(3<2)
+
+console.log(100==4)
+console.log(100!=4)
+
