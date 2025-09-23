adbf0db63e886761e3dae3f725c5a9abfcd02e76
Geyek Ulrich Armel <ulrichsgraphix@gmail.com>
2025-09-23 17:16:08 +0100

    added more content on inheritance

--- Day5/inheritantance.js
+++ Day5/inheritantance.js
@@ -10,3 +10,8 @@ console.log("bird1 can fly:", bird1.canFly)
 console.log("bird1 has feather :", bird1.hasFeather)
 console.log("bird1 has wings :", bird1.hasWings)
 
+var pingue1 = Object.create(bird)
+console.log("pingue1 result, after modification of inheritance of bird")
+console.log(pingue1.canFly = false)
+console.log(pingue1.hasFeather = 30)
+console.log(pingue1.hasWings ="it does not has wings")
