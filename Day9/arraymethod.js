a77a0de96adda94822c7c6425a3386dafab3ed71
Geyek Ulrich Armel <ulrichsgraphix@gmail.com>
2025-09-30 11:40:17 +0100

    maniputing array method like pop and push

---
+++ Day9/arraymethod.js
@@ -0,0 +1,18 @@
+
+// manipulating array
+
+let array = [1, 2, 3, 4]
+array.push(4, 4, 7)
+    console.log(array)
+
+let messages = [
+    "Hey, how's it going?",        
+    "I'm great, thank you! How about you?",
+    "All good. Been working on my portfolio lately."]
+
+let newMessage = "Same here!"
+
+messages.push(newMessage)
+console.log(messages)
+messages.pop()
+console.log(messages)
--- Day9/blackjac.js
+++ Day9/blackjac.js
@@ -1,22 +1,48 @@
-let firstCard = 9
-let secondCard = 12
-let sum = firstCard + secondCard 
+let firstCard = 10
+let secondCard = 9
+let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
-let message = ""
+
+let card = [firstCard, secondCard]
 
 
-// 1. Declare a variable called message and assign its value to an empty string
+let messageEl = document.getElementById("message-el")
+let sumEl = document.querySelector("#sum-el")
+let  cardEl = document.querySelector("#card-el")
 
-// 2. Reassign the message variable to the string we're logging out
-if (sum <= 20) {
-    message = "Do you want to draw a new card? "
-} else if (sum === 21) {
-    message= "Wohoo! You've got Blackjack! "
-    hasBlackJack = true
-} else {
-    message= "You're out of the game! "
-    isAlive = false
+
+function startGame(){
+    renderGame()
 }
 
-console.log(message)
+function renderGame() {
+    if (sum <= 20) {
+        sumEl.textContent = `Sum: ${firstCard + secondCard}`
+        messageEl.textContent = "Do you want to draw a new card? "
+        cardEl.textContent = `Card: ${card[0]} ${card[1]}`
+    } else if (sum === 21) {
+        sumEl.textContent = `Sum: ${card[0] + card[1]}`
+        messageEl.textContent = "Wohoo! You've got Blackjack! "
+        cardEl.textContent = `Card: ${firstCard} ${secondCard}`
+        hasBlackJack = true
+    } else {
+        messageEl.textContent = "You're out of the game! "
+        sumEl.textContent = `Sum: ${card[0] + card[1]}`
+        cardEl.textContent = `Card: ${card[0]} ${card[1]}`
+        isAlive = false
+    }
+   
+}
+
+
+function  newCard(){
+    let card = 1
+    sum += card
+    renderGame()
+    sumEl.textContent = `Sum : ${sum}`
+}
+
+
+
+
