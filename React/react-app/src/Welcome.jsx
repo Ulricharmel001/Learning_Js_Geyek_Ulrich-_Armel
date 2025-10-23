46ff399ee73c014dc67facd55be48806f4b4a90a
Ulrich-armel <ulrichsgraphix@gmail.com>
2025-10-23 17:20:41 +0100

    made use of conditional rendering in  Welcome.jsx file with the funtion that takes props

--- React/react-app/src/Welcome.jsx
+++ React/react-app/src/Welcome.jsx
@@ -1,12 +1,26 @@
+import propTypes from "prop-types";
+
 function Welcome(props){
 //     if (props.isLogedin ){
 //         return <h2>Welcome {props.username}</h2>
 //     }
 //     else{return <h2>Please login to continue</h2>} 
-// }
+const welcome =  <h2 className="welcome">Welcome {props.username}</h2>
+const please =  <h2 className="please">Please login to continue</h2>
 
-return(props.isLogedin ?  <h2>Welcome {props.username}</h2> :
-                     <h2>Please login to continue</h2>);
-}
+
+
+return(props.isLogedin ? welcome : please );
+}                     
 
 export default Welcome
+Welcome.prototype ={
+    username : propTypes.string,
+    isLogedin : propTypes.bool
+
+
+}
+Welcome.defaultProps ={
+    isLogedin : false,
+    username: "Guest"
+}
